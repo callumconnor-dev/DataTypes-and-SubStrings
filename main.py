b635f41894925e415be2b117fc018a9b9b1fc68b
callumconnor-dev <132172088+callumconnor-dev@users.noreply.github.com>
2023-07-09 18:59:19 +0100

    Type Checking

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ print("The initial of your first name is " + firstName[0]) # Substrings locate a
 print("The last letter of your name is " + firstName[len(firstName) - 1]) # Substrings don't have to be exact numbers, the numbers can be determined by functions, like len()
 # Substrings count from 0 
 
+# Printing Types
+print(firstName + " is a " + str(type(firstName)))
+
 # Integer
 print("123" + "456") # Strings are determined using "", the output of this code will be 123456
 print(123 + 456) # Integers do not need "", integers are only whole numbers, the output of this code would be 579
 
 # Float
 pi = 3.14159 # Floats are initialized like integers, except Floats must contain a point -> . <- somewhere in the number
-print(pi)
+print(str(pi) + " is a " + str(type(pi)))
 
 # Boolean
 earthRound = True # Booleans come in two states, True or False
